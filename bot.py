ea018c0db80df8dd68a2049ab2b1b05dce3f1299
sderamon <sderamon@gmail.com>
2026-07-29 11:58:23 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,22 @@
+from dates import generate_trip_dates
 from telegram_bot import send_message
 
-send_message("✅ Bot iniciado correctamente")
+def main():
+    viajes = generate_trip_dates()
+
+    mensaje = "🚄 Buscando ofertas Barcelona → Madrid\n\n"
+
+    mensaje += f"Viajes a comprobar: {len(viajes)}\n\n"
+
+    mensaje += "Primeros viajes:\n"
+
+    for viaje in viajes[:5]:
+        mensaje += (
+            f"{viaje['outbound']} ➜ {viaje['inbound']}\n"
+        )
+
+    send_message(mensaje)
+
+
+if __name__ == "__main__":
+    main()
