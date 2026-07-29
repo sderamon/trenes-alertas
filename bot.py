eab5cf12f7913098633722279756d8ab58e2088d
sderamon <sderamon@gmail.com>
2026-07-29 11:56:51 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,34 +1,3 @@
-from playwright.sync_api import sync_playwright
+from telegram_bot import send_message
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=True)
-
-    page = browser.new_page(viewport={"width": 1440, "height": 1200})
-
-    page.goto(
-        "https://www.ouigo.com/es",
-        wait_until="domcontentloaded",
-        timeout=120000
-    )
-
-    page.wait_for_timeout(5000)
-
-    print(page.title())
-    print(page.url)
-
-    print("\nINPUTS ENCONTRADOS:\n")
-
-    for i, locator in enumerate(page.locator("input").all()):
-        print(i, locator.get_attribute("type"), locator.get_attribute("placeholder"))
-
-    print("\nTEXTBOX:\n")
-
-    for i, locator in enumerate(page.get_by_role("textbox").all()):
-        print(i)
-
-    print("\nCOMBOBOX:\n")
-
-    for i, locator in enumerate(page.get_by_role("combobox").all()):
-        print(i)
-
-    browser.close()
+send_message("✅ Bot iniciado correctamente")
